771a453e3414d984415632441e2b0e0a3e063974
Amin A <amin.asdzdh@gmail.com>
2021-09-30 11:56:18 +1000

    fix: bug while handling duplicate names

--- scripts/dataset_manipulation.py
+++ scripts/dataset_manipulation.py
@@ -17,43 +17,34 @@ def merge_datasets(ds_1_dir, ds_2_dir, ds_merged_dir):
     Path(os.path.join(ds_merged_dir, 'labels')).mkdir(parents=True, exist_ok=True)
     
     # copy images to target folder
+    new_img_ids = [str(i) + '.jpg' for i in range(len(ds_1))]
     for i in tqdm(range(len(ds_1))):
         src = os.path.join(ds_1_dir, 'images', ds_1.iloc[i]['image_id'])
-        dst = os.path.join(ds_merged_dir, 'images', ds_1.iloc[i]['image_id'])
-        copyfile(src, dst+'temp')
+        dst = os.path.join(ds_merged_dir, 'images', new_img_ids[i])
+        copyfile(src, dst)
+        ds_1.at[i, 'image_id'] = new_img_ids[i]
 
+    new_img_ids = [str(i+len(ds_1)) + '.jpg' for i in range(len(ds_2))]
     for i in tqdm(range(len(ds_2))):
         src = os.path.join(ds_2_dir, 'images', ds_2.iloc[i]['image_id'])
-        dst = os.path.join(ds_merged_dir, 'images', ds_2.iloc[i]['image_id'])
+        dst = os.path.join(ds_merged_dir, 'images', new_img_ids[i])
         copyfile(src, dst)
-        os.rename(dst, dst+'temp')
-    
+        ds_2.at[i, 'image_id'] = new_img_ids[i]
+
     # merge labels
     ds_merged = pd.concat([ds_1, ds_2]).reset_index(drop=True)
     # shuffle
     ds_merged = ds_merged.sample(frac=1).reset_index(drop=True)
 
-    # generate new img names
-    new_img_ids = [str(i) + '.jpg' for i in range(len(ds_merged))]
-
     print(ds_merged.head())
-
-    # update names
-    for i in range(len(ds_merged)):
-        old_id = ds_merged.iloc[i]['image_id']
-        new_id = new_img_ids[i]
-        # rename file
-        os.rename(os.path.join(ds_merged_dir, 'images', old_id)+'temp', os.path.join(ds_merged_dir, 'images', new_id))
-        # rename in table
-        ds_merged.at[i, 'image_id'] = new_id
-
-    print(ds_merged.head())
+    
     ds_merged.to_csv(os.path.join(ds_merged_dir, 'labels', 'labels.csv'), index=False)
 
 
 if __name__ == "__main__":
-    ds_1 = os.path.join(os.getcwd(), 'datasets', 'eval', 'being_prepared', 'org+HF+IC+RR')
-    ds_2 = os.path.join(os.getcwd(), 'datasets', 'eval', 'being_prepared', 'RealSet_RandomTranslate')
-    ds_merged = os.path.join(os.getcwd(), 'datasets', 'eval', 'being_prepared', 'org+HF+IC+RR+RT')
+
+    ds_1 = os.path.join('E:', os.sep, 'My Datasets', 'Excavator_Pose_2D', 'AutCon2020', 'Archive', 'Experiment_2', 'prepared_datasets', '8')
+    ds_2 = os.path.join('E:', os.sep, 'My Datasets', 'Excavator_Pose_2D', 'AutCon2020', 'Archive', 'Experiment_2', 'prepared_datasets', '9')
+    ds_merged = os.path.join('E:', os.sep, 'My Datasets', 'Excavator_Pose_2D', 'AutCon2020', 'Archive', 'Experiment_2', 'prepared_datasets', '10')
     
     merge_datasets(ds_1, ds_2, ds_merged)
